da0f1704bee0cac28fb98d6048c218e506cf794e
Jonathan Martel <jonathmartel@gmail.com>
2021-09-14 15:36:57 -0400

    avec le fetch et le setState

--- src/components/ListeProduits/ListeProduits.js
+++ src/components/ListeProduits/ListeProduits.js
@@ -4,20 +4,36 @@ import "./ListeProduits.css";
 export default class ListeProduits extends React.Component{
     constructor(props){
         super(props);
+        this.state = {
+            items :[]
+        }
     }
 
     componentDidMount(){
         fetch("http://127.0.0.1:8000/webservice/php/biere")
             .then(reponse => reponse.json())
-            .then((donnees)=>{console.log(donnees)});
+            .then((donnees)=>{
+                this.setState({items:donnees.data}) 
+                console.log(donnees)
+            });
+    }
+
+    cliquer(){
+        console.log(this);
+        console.log("click");
     }
 
     render(){
+        const produits = this.state.items.map((item, index)=>{
+            return (
+                <article onClick={this.cliquer.bind(item)} key={index}>{item.nom}</article>
+            );
+        })
+        console.log(produits);
         return (
             <section>
-                <article>1</article>
-                <article>2</article>
-                <article>3</article>
+                <p>Nombre d'item(s) : {this.state.items.length}</p>
+                {produits}
             </section>
         );
     }
