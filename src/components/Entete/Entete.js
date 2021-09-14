ddf130ea0e04bdcef1f1158bbd13fc659733910e
Jonathan Martel <jonathmartel@gmail.com>
2021-09-14 15:02:35 -0400

    Ajout du composant ListeProduits

--- src/components/Entete/Entete.js
+++ src/components/Entete/Entete.js
@@ -2,25 +2,38 @@ import React from 'react';
 import BoutonNav from '../BoutonNav/BoutonNav';
 import './Entete.css';
 
-export default class Entete extends React.Component{
+export default class Entete extends React.Component {
   constructor(props){
     super(props);
     console.log(this.props.titre);
     //this.props.titre = "Titre par défaut";  // Lecture seule
-    this.compte = 10;
+    //this.compte = 10;
+    this.state = {compte:10}; // Le state contient les données du composant.
+    this.boutonCliquer = this.boutonCliquer.bind(this);
   }
 
   boutonCliquer(){
       console.log("Click");
       console.log(this);
-      //cd this.compte++;
+      //this.setState({compte:this.state.compte+1});  // À ne pas faire, parce que le setState est asynchrone!
+      
+      /*this.setState(function(state, props){
+                      return {
+                        compte : state.compte+1
+                      };
+                    });*/
+      this.setState((state)=>({compte:state.compte+1}))
   }
 
   render() {
+    // Il peut y avoir du code ici...
+    const titre = this.props.titre || "titre par défaut";
+    
     return (  
             <div>
-                <h1 className="test">{this.props.titre}</h1>
-                <button onClick={this.boutonCliquer}>Cliquez ici ({this.compte})</button>
+                <h1 className="test">{titre}</h1>
+                <h1 className="test">{this.props.titre || "titre par défaut"}</h1>
+                <button onClick={this.boutonCliquer}>Cliquez ici ({this.state.compte})</button>
                 <nav>
                     <ul>
                         <li><BoutonNav lien="#1" label="Item 1"/></li>
---
+++ src/components/ListeProduits/ListeProduits.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./ListeProduit.css";
+
+export default class ListeProduits extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <section>
+                <article>1</article>
+                <article>2</article>
+                <article>3</article>
+            </section>
+        );
+
+    }
+}
